145b1c26008e0402ffc703f9f3929ea2d2b29cb5
root <duyixuan@jinhuwuliang.com>
2023-06-06 16:56:45 +0800

    update the speed_test

--- test/speed_test.cc
+++ test/speed_test.cc
@@ -9,10 +9,28 @@ inline uint64_t rdtsc() {
 }
 #define CPU_FRE (2800000000)
 
+class TestNode {
+public:
+  TestNode() = default;
+  TestNode(int v, std::string str) : m_value(v), m_str(str) {}
+  TestNode(const TestNode &other)
+      : m_value(other.m_value), m_str(other.m_str) {}
+  void print() {
+    std::cout << "The m_value is " << m_value << " The m_str is " << m_str
+              << std::endl;
+  }
+  ~TestNode() = default;
+
+public:
+  int m_value;
+  std::string m_str;
+};
+
 void test_push_back()
 {
-    std::cout << "Test push_back 10000 elems" << std::endl;
-    int count = 10000;
+    
+    int count = 1000000;
+    std::cout << "Test push_back " << count <<  " elems" << std::endl;
     std::vector<int> tmp_vec;
     uint64_t vec_begin;
     uint64_t vec_end;
@@ -36,72 +54,73 @@ void test_push_back()
     double seg_vec_cost;
     vec_cost = (vec_end - vec_begin)/double(CPU_FRE);
     seg_vec_cost = (seg_vec_end - seg_vec_begin)/double(CPU_FRE);
-    std::cout << "vector push_back 10000 elems costs " << vec_cost << std::endl;
-    std::cout << "segment_vector push_back 10000 elems costs " << seg_vec_cost << std::endl;
+    std::cout << "vector push_back "<< count << " elems costs " << vec_cost << std::endl;
+    std::cout << "segment_vector push_back " << count <<" elems costs " << seg_vec_cost << std::endl;
 }
 
 void test_emplace_back()
 {
-    std::cout << "Test emplace_back 10000000 elems" << std::endl;
-    int count = 1000;
-    std::vector<int> tmp_vec;
+    
+    int count = 1000000;
+    std::cout << "Test emplace_back " << count << " elems" << std::endl;
+    std::vector<TestNode> tmp_vec;
     uint64_t vec_begin;
     uint64_t vec_end;
     vec_begin = rdtsc();
     for (int i = 0; i < count; ++i)
     {
-        tmp_vec.emplace_back(i);
+        tmp_vec.emplace_back(TestNode(i, "hello"));
     }
     vec_end = rdtsc();
 
-    segment_vector<int> tmp_seg_vec;
+    segment_vector<TestNode> tmp_seg_vec;
     uint64_t seg_vec_begin;
     uint64_t seg_vec_end;
     seg_vec_begin = rdtsc();
     for (int i = 0; i < count; ++i)
     {
-        tmp_seg_vec.emplace_back(i);
+        tmp_seg_vec.emplace_back(TestNode(i, "hello"));
     }
     seg_vec_end = rdtsc();    
     double vec_cost;
     double seg_vec_cost;
     vec_cost = (vec_end - vec_begin)/double(CPU_FRE);
     seg_vec_cost = (seg_vec_end - seg_vec_begin)/double(CPU_FRE);
-    std::cout << "vector emplace_back 10000 elems costs " << vec_cost << std::endl;
-    std::cout << "segment_vector emplace_back 10000 elems costs " << seg_vec_cost << std::endl;    
+    std::cout << "vector emplace_back " << count << " elems costs " << vec_cost << std::endl;
+    std::cout << "segment_vector emplace_back " << count << " elems costs " << seg_vec_cost << std::endl;    
 }
 
 
 void test_resize()
 {
-    std::cout << "Test resize 100000 elems" << std::endl;
     int count = 10000000;
-    std::vector<int> tmp_vec;
+    std::cout << "Test resize "<< count << " elems" << std::endl;
+    std::vector<TestNode> tmp_vec;
     uint64_t vec_begin;
     uint64_t vec_end;
     vec_begin = rdtsc();
-    tmp_vec.resize(10000, 10);
+    tmp_vec.resize(count, TestNode(1, "hello"));
     vec_end = rdtsc();
 
-    segment_vector<int> tmp_seg_vec;
+    segment_vector<TestNode> tmp_seg_vec;
     uint64_t seg_vec_begin;
     uint64_t seg_vec_end;
     seg_vec_begin = rdtsc();
-    tmp_seg_vec.resize(10000, 10);
+    tmp_seg_vec.resize(count, TestNode(1, "hello"));
     seg_vec_end = rdtsc();    
     double vec_cost;
     double seg_vec_cost;
     vec_cost = (vec_end - vec_begin)/double(CPU_FRE);
     seg_vec_cost = (seg_vec_end - seg_vec_begin)/double(CPU_FRE);
-    std::cout << "vector resize 10000 elems costs " << vec_cost << std::endl;
-    std::cout << "segment_vector resize 10000 elems costs " << seg_vec_cost << std::endl;    
+    std::cout << "vector resize "<< count << " elems costs " << vec_cost << std::endl;
+    std::cout << "segment_vector resize "<< count << " elems costs " << seg_vec_cost << std::endl;    
 }
 
 
 void test_traverse()
 {
-    std::cout << "Test push_back 10000 elems" << std::endl;
-    int count = 10000;
+    int count = 1000000;
+    std::cout << "Test traverse "<< count << " elems" << std::endl;
     std::vector<int> tmp_vec(count, 10);
     uint64_t vec_begin;
     uint64_t vec_end;
@@ -132,11 +151,79 @@ void test_traverse()
 }
 
 
+void test_pop_back()
+{
+    int count = 1000000;
+    std::cout << "Test pop_back " << count << " elems" << std::endl;
+    std::vector<TestNode> tmp_vec(count, TestNode(1, "hello"));
+    uint64_t vec_begin;
+    uint64_t vec_end;
+    vec_begin = rdtsc();
+    for (int i = 0; i < count; ++i)
+    {
+        tmp_vec.pop_back();
+    }
+    vec_end = rdtsc();
+
+    segment_vector<TestNode> tmp_seg_vec(count, TestNode(1, "hello"));
+    uint64_t seg_vec_begin;
+    uint64_t seg_vec_end;
+    seg_vec_begin = rdtsc();
+    for (int i = 0; i < count; ++i)
+    {
+        tmp_seg_vec.pop_back();
+    }
+    seg_vec_end = rdtsc();    
+    double vec_cost;
+    double seg_vec_cost;
+    vec_cost = (vec_end - vec_begin)/double(CPU_FRE);
+    seg_vec_cost = (seg_vec_end - seg_vec_begin)/double(CPU_FRE);
+    std::cout << "vector pop_back " << count << " elems costs " << vec_cost << std::endl;
+    std::cout << "segment_vector pop_back " << count << " elems costs " << seg_vec_cost << std::endl;      
+}
+
+void test_assign_val()
+{
+    int count = 10000000;
+    int N = 10;
+    std::cout << "Test assign " << count << " elems" << std::endl;
+    std::vector<TestNode> tmp_vec;
+    uint64_t vec_begin;
+    uint64_t vec_end;
+    vec_begin = rdtsc();
+    for (int i = 0; i < N; ++i)
+    {
+        tmp_vec.assign(count, TestNode(i, "hello"));
+    }
+    vec_end = rdtsc();
+
+    segment_vector<TestNode> tmp_seg_vec;
+    uint64_t seg_vec_begin;
+    uint64_t seg_vec_end;
+    seg_vec_begin = rdtsc();
+    for (int i = 0; i < N; ++i)
+    {
+        tmp_seg_vec.assign(count, TestNode(i, "hello"));
+    }
+    seg_vec_end = rdtsc();    
+    double vec_cost;
+    double seg_vec_cost;
+    vec_cost = (vec_end - vec_begin)/double(CPU_FRE)/N;
+    seg_vec_cost = (seg_vec_end - seg_vec_begin)/double(CPU_FRE)/N;
+    std::cout << "vector assign " << count << " elems costs " << vec_cost << std::endl;
+    std::cout << "segment_vector assign " << count << " elems costs " << seg_vec_cost << std::endl;  
+}
+
+
+
 int main()
 {
-    test_push_back();
-    test_emplace_back();
-    test_resize();
-    test_traverse();
+    // test_push_back();
+    // test_emplace_back();
+    // test_resize();
+    // test_traverse();
+    // test_reserve();
+    // test_pop_back();
+    // test_assign_val();
     return 0;
 }
